a730bc74731c4f1b45e1e5ccdb170307804e503e
Dave Welch <david@davidwelch.co>
2018-03-24 08:27:00 -0600

    change

--- src/db/V1_6__foundation_views.sql
+++ src/db/V1_6__foundation_views.sql
@@ -3,7 +3,7 @@ create or replace view api.identity as (
     i.id, i.tenant_id,
     im.member_id,
     m.name, m.display_name, m.mrn, m.slug, m.tenant_oid
-  from iam.identity i
-    inner join iam.identity_member im on im.identity_id = i.id
+  from iam.identity_member im
+    inner join iam.identity i on im.identity_id = i.id
     inner join membership.member m on m.id = im.member_id
 );
